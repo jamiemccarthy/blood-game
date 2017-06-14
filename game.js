48afd92cc225525b3f557238c23c0f4a936a1acd
Becca Barton <becca.barton@voxmedia.com>
2017-06-14 16:13:34 -0400

    add start screen

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
 var game = new Phaser.Game(1000, 650, Phaser.AUTO, '');
 
-// add states but hold off on turning on pregame state for now so it isnt hell to develop
-// game.state.add('pregame', pregameState);
+game.state.add('pregame', pregameState);
 game.state.add('play', playState);
 game.state.add('end', endState);
-//change this to pregame later
-game.state.start('play');
+
+game.state.start('pregame');
