6315b8e721a110804a6b53913ba895da814db644
Becca Barton <becca.barton@voxmedia.com>
2017-06-15 11:24:38 -0400

    add end state

--- end-state.js
+++ end-state.js
@@ -1,8 +1,20 @@
+var title;
+
 var endState = {
+  preload: function() {
+    game.load.spritesheet('title', 'img/end_screen/game_over_spritesheet.png', 528, 275);
+    game.load.image('background', 'img/end_screen/bg.png');
+  },
+
   create: function() {
-    var skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
-    var startGraphic = game.add.text(80,200, "Press 'S' to restart", {font: "25px VT323", fill: "red"});
-    skey.onDown.addOnce(this.restart, this);
+    var rkey = game.input.keyboard.addKey(Phaser.Keyboard.R);
+    game.add.sprite(0, 0, 'background');
+    title = game.add.sprite(game.world.centerX - 263, 100, 'title');
+    game.add.text(game.world.centerX - 95, 450, "press 'r' to restart", {font: "25px VT323", fill: "white"});
+    title.animations.add('blink', [0, 1], 2, true);
+    title.animations.play('blink');
+
+    rkey.onDown.addOnce(this.restart, this);
   },
 
   restart: function() {
